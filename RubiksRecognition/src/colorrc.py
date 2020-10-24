e60e867294849edccc2eaf7d9e195620fae9c8ad
Kawa-git <56511237+Kawa-git@users.noreply.github.com>
2020-10-24 22:55:55 +0200

    adding color

--- RubiksRecognition/src/colorrc.py
+++ RubiksRecognition/src/colorrc.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import csv
 
+
+
 def nothing(x): pass
 
 '''
@@ -17,9 +19,52 @@ def start():
     file = open("./RubiksRecognition/resources/ranges.csv", 'r')
     colorRanges = csv.reader(file)
 
+    #assigning to every ranges a value (-2 = error)
+    white_range = [-2,-2,-2,-2,-2,-2,-2]
+    blue_range = [-2,-2,-2,-2,-2,-2,-2]
+    yellow_range = [-2,-2,-2,-2,-2,-2,-2]
+    green_range = [-2,-2,-2,-2,-2,-2,-2]
+    orange_range = [-2,-2,-2,-2,-2,-2,-2]
+    red_range = [-2,-2,-2,-2,-2,-2,-2]
+
+    #getting ranges
+    for row in colorRanges:
+        if row[0] == str(0):
+            white_range = row[1:]
+        elif row[0] == str(1):
+            blue_range = row[1:]
+        elif row[0] == str(2):
+            yellow_range = row[1:]
+        elif row[0] == str(3):
+            green_range = row[1:]
+        elif row[0] == str(4):
+            orange_range = row[1:]
+        elif row[0] == str(5):
+            red_range = row[1:]
+        
+    #si ok va ottimizzato, non riesco a pensare al momento AHAHA
+    lw_mask = np.array([int(white_range[0]), int(white_range[1]), int(white_range[2])])
+    uw_mask = np.array([int(white_range[3]), int(white_range[4]), int(white_range[5])])
+
+    lo_mask = np.array([int(orange_range[0]), int(orange_range[1]), int(orange_range[2])])
+    uo_mask = np.array([int(orange_range[3]), int(orange_range[4]), int(orange_range[5])])
+
+    ly_mask = np.array([int(yellow_range[0]), int(yellow_range[1]), int(yellow_range[2])])
+    uy_mask = np.array([int(yellow_range[3]), int(yellow_range[4]), int(yellow_range[5])])
+
+    lg_mask = np.array([int(green_range[0]), int(green_range[1]), int(green_range[2])])
+    ug_mask = np.array([int(green_range[3]), int(green_range[4]), int(green_range[5])])
+
+    lb_mask = np.array([int(blue_range[0]), int(blue_range[1]), int(blue_range[2])])
+    ub_mask = np.array([int(blue_range[3]), int(blue_range[4]), int(blue_range[5])])
+    
+    lr_mask = np.array([int(red_range[0]), int(red_range[1]), int(red_range[2])])
+    ur_mask = np.array([int(red_range[3]), int(red_range[4]), int(red_range[5])])
+
     frameWidth = 680 #680
     frameHeight = 480 #480
 
+    #0 default camera
     cap = cv2.VideoCapture(0)
 
     cap.set(3, frameWidth)
@@ -35,7 +80,9 @@ def start():
 
     while True:
         success, img = cap.read()
+
         img = img[65:240, 230:395]
+        
         imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         l_h = cv2.getTrackbarPos("L - H", "Trackbars")
@@ -51,11 +98,31 @@ def start():
         lower = np.array([l_h,l_s,l_v])
         upper = np.array([u_h, u_s, u_v])
 
-        mask = cv2.inRange(imgHSV, lower, upper)
-        #mask = mask[65:240, 230:395]
+        #mask = cv2.inRange(imgHSV, lower, upper)
+
+        white_mask = cv2.inRange(imgHSV, lw_mask, uw_mask)
+        red_mask = cv2.inRange(imgHSV, lr_mask, ur_mask)
+        orange_mask = cv2.inRange(imgHSV, lo_mask, uo_mask)
+        yellow_mask = cv2.inRange(imgHSV, ly_mask, uy_mask)
+        blue_mask = cv2.inRange(imgHSV, lower, upper)
+        green_mask = cv2.inRange(imgHSV, lg_mask, ug_mask)
+
+        full_mask = white_mask + red_mask + orange_mask + yellow_mask + blue_mask + green_mask
+
+        full_colored_mask = cv2.bitwise_and(img, img, mask=full_mask)
+
+        #pixel smoothing
+        blurred_full_mask = cv2.medianBlur(full_colored_mask, 15)
+    
+        
         
         cv2.imshow("Result", img)
-        cv2.imshow("mask", mask)
+        cv2.imshow("white mask", blurred_full_mask)
+
+
+        #cv2.imshow("mask", mask)
+        #cv2.imshow("colored mask", colored_mask)
+        #cv2.imshow("median blur", median_blur)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):   break
     file.close()
