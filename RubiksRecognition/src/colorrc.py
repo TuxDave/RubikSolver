dcd4aec53b19cfa334babff70177688d9446aca4
Kawa-git <56511237+Kawa-git@users.noreply.github.com>
2020-10-26 17:07:22 +0100

    Riconoscimento funzionante mk1

--- RubiksRecognition/src/colorrc.py
+++ RubiksRecognition/src/colorrc.py
@@ -9,6 +9,7 @@ def nothing(x): pass
 FROM_INT_TO_COLOR = {0: "white",1: "blue",2: "yellow",3: "green",4: "orange",5: "red"}
 CUBE_LENGTH = 58
 BOUNDS = [((x)*CUBE_LENGTH, (y)*CUBE_LENGTH) for x in range(3) for y in range(3)]
+print(BOUNDS)
 CHECK_POINT = [(29,29)]
 
 def start(color):
@@ -19,7 +20,7 @@ def start(color):
     frameWidth = 680 #680
     frameHeight = 480 #480
 
-    cap = cv2.VideoCapture("RubiksRecognition/notTracked/cube1.mp4")
+    cap = cv2.VideoCapture(0)
 
     cap.set(3, frameWidth)
     cap.set(4, frameHeight)
@@ -30,7 +31,7 @@ def start(color):
             img = img[65:240, 230:405] #175*175
         except:
             cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-            success, img = cap.read()
+            success, img = cap.read()            
         imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         
         cv2.imshow("Result", img)
@@ -43,7 +44,11 @@ def start(color):
 #prende un immagine in HSV e restituisce un array di numeri che sono i colori
 def fromImageToColorSequence(img, ranges):
     pos = {}
+    for i in range(9):
+        pos[i] = None
+
     cropped = 0
+
     for c in FROM_INT_TO_COLOR.keys():#per ogni colore
 
         #dopo questo blocco color conterrà il range per il colore corrente
@@ -59,7 +64,7 @@ def fromImageToColorSequence(img, ranges):
         mask = cv2.inRange(img, np.array([color[1], color[2], color[3]]), np.array([color[4], color[5], color[6]]))
 
         for i in range(9):
-            cropped = mask[BOUNDS[i][1]:BOUNDS[i][1]+CUBE_LENGTH, BOUNDS[i][0]:BOUNDS[i][0]+CUBE_LENGTH]
+            cropped = mask[BOUNDS[i][0]:BOUNDS[i][0]+CUBE_LENGTH, BOUNDS[i][1]:BOUNDS[i][1]+CUBE_LENGTH]
             found = True
             for couple in CHECK_POINT:
                 if cropped[couple[1], couple[0]] != 255:
@@ -70,19 +75,18 @@ def fromImageToColorSequence(img, ranges):
     pos = ordinaDict(pos)
 
     pos2 = {}
-    for i in range(9):
-        try:
-            if i == 0:
-                pos2[0] = pos[4]
-            elif i < 5:
-                pos2[i] = pos[i-1]
-            else:
-                pos2[i] = pos[i]
-        except:
-            pos2[i] = None
-                
+    pos2[0] = pos[4]
+    pos2[1] = pos[0]
+    pos2[2] = pos[1]
+    pos2[3] = pos[2]
+    pos2[4] = pos[5]
+    pos2[5] = pos[8]
+    pos2[6] = pos[7]
+    pos2[7] = pos[6]
+    pos2[8] = pos[3]
 
     return ordinaDict(pos2)
+    
 
 def ordinaDict(dict1):
     ret = {}
