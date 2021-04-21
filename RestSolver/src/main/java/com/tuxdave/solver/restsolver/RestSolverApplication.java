de96fcc271dba72ddc0f0d347dbea22e9320303c
TuxDave <godinodavide@gmail.com>
2021-04-21 16:43:36 +0200

    created the base structure of spring web REST

---
+++ RestSolver/src/main/java/com/tuxdave/solver/restsolver/RestSolverApplication.java
@@ -0,0 +1,13 @@
+package com.tuxdave.solver.restsolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSolverApplication.class, args);
+    }
+
+}
---
+++ RestSolver/src/test/java/com/tuxdave/solver/restsolver/RestSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuxdave.solver.restsolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSolverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
